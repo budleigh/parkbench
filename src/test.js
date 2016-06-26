238bcdd2dfe281da646ab0cb5f7875e2b18bc1f2
budleigh <arguewithplato@gmail.com>
2016-06-25 20:02:18 -0700

    fixed test issues, basic sorting test

--- src/test.js
+++ src/test.js
@@ -7,118 +7,54 @@
 
 const array = require('./array.js');
 const util = require('./util.js');
-const _ = require('../node_modules/underscore/underscore.js');
 
 var ARRAYSIZE = 5000;
 
-function benchMarkSort (fn, arraySize, genCaseIterations) {
-  /*
-  A crude benchmarking of the different sorting algorithms.
-  Obviously, the most 'typical' use case is a randomized
-  (shuffled) array. however, the performance of sorting
-  algorithms on other edge cases, such as full-equal,
-  already-sorted, and reverse-sorted arrays is also notable.
-  */
-
-  console.log('Testing ' + fn.name);
-
-  try {
-    testSort(fn);
-  } catch (err) {
-    console.log('' + fn.name + ' errored out: ' + err + '\n\n');
-    return;
-  }
-
-  if (!(util.verifySorted(fn(util.genRandomArray(arraySize))))) {
-    console.log('\tfailed!\n\n');
-    return;
-  } else {
-    console.log('\tpassed!');
-  }
-
-  // our test arrays, generated fresh-baked for each function
-  var sortedArray = util.genSortedArray(arraySize),
-      randomArray = util.genRandomArray(arraySize),
-      equalsArray = util.genEqualsArray(arraySize),
-      reversedArray = util.genReversedArray(arraySize);
-
-  // run 'em
-  var sortedTime = util.timeIt(fn, null, sortedArray),
-      randomTime = util.timeIt(fn, null, randomArray),
-      equalsTime = util.timeIt(fn, null, equalsArray),
-      reversedTime = util.timeIt(fn, null, reversedArray);
-
-  console.log('\t\tsorted array completed in ' + sortedTime + ' seconds');
-  console.log('\t\trandom array completed in ' + randomTime + ' seconds');
-  console.log('\t\tequals array completed in ' + equalsTime + ' seconds');
-  console.log('\t\treversed array completed in ' + reversedTime + ' seconds');
-
-  // because this is the general use-case, let's run a number of tests to
-  // get a good idea of how it performs, just in case one of our randomly
-  // generated arrays lies too closely to an edge case.
-  var iter = typeof genCaseIterations !== "number" ? 10 : genCaseIterations;
-  var acc = 0;
-  for (var x = 0; x < iter; x++) {
-    var array = util.genRandomArray(arraySize);
-    acc += util.timeIt(fn, null, array);
-  }
-  var avg = (acc / iter);
-  console.log('\t\tsorted ' + iter + ' random arrays in an average of ' + (acc / iter) + ' seconds\n\n');
-
-  return avg;
-}
-
 function testSort (fn) {
-  var randomArray = util.genRandomArray(ARRAYSIZE),
-      sortedArray = util.genSortedArray(ARRAYSIZE),
-      equalsArray = util.genEqualsArray(ARRAYSIZE),
-      reversedArray = util.genReversedArray(ARRAYSIZE),
-      emptyArray = [];
+  var arrays = {
+    empty: [],
+    sorted: util.genSortedArray(ARRAYSIZE),
+    equals: util.genEqualsArray(ARRAYSIZE),
+    random: util.genRandomArray(ARRAYSIZE),
+    reversed: util.genReversedArray(ARRAYSIZE)
+  };
+  var errors = [];
 
-  fn(randomArray);
-  fn(sortedArray);
-  fn(equalsArray);
-  fn(reversedArray);
-  fn(emptyArray);
-
-  if (!util.verifySorted(randomArray) ||
-      !util.verifySorted(sortedArray) ||
-      !util.verifySorted(equalsArray) ||
-      !util.verifySorted(reversedArray) ||
-      !util.verifySorted(emptyArray)) {
-    return false;
+  for (var arrayType in arrays) {
+    try {
+      var sorted = util.verifySorted(fn(arrays[arrayType]));
+      if (!sorted) {
+        errors.push(fn.name + ' failed to sort array type: ' + arrayType);
+      }
+    } catch (err) {
+      errors.push(fn.name + ' errored out on array type: ' + arrayType + ': ' + err);
+    }
   }
 
-  return true;
+  if (errors.length === 0) {
+    console.log(fn.name + ' passed!');
+  } else {
+    console.log(fn.name + ' failed: ');
+    console.log(errors);
+  }
 }
 
 function runFromTerminal () {
   switch (process.argv[2]) {
+
     case 'bench':
-      if (process.argv[3] !== undefined) {
-        var arrsizeArg = parseInt(process.argv[3]);
-        ARRAYSIZE = typeof arrsizeArg === 'number' ? arrsizeArg : ARRAYSIZE;
-
-        if (ARRAYSIZE > 75000) {
-          console.log('you put in a big number for the test array size.\nmight take a while!');
-        }
-      }
-      _.each(array, (fn) => {
-        benchMarkSort(fn, ARRAYSIZE, 10);
-      });
       break;
-    default:
-      console.log('unknown argument: ' + process.argv[2]);
-  }
-}
 
-function computeMaxCallStackSize() {
-    try {
-        return 1 + computeMaxCallStackSize();
-    } catch (e) {
-        // Call stack overflow
-        return 1;
-    }
+    case 'test':
+      for (var fn in array) {
+        testSort(array[fn]);
+      }
+      break;
+
+    default:
+      console.log('unknown argument');
+      break;
+  }
 }
 
 runFromTerminal();
